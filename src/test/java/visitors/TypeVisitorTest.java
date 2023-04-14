9b829ccfc46c3c2f7a3f346076c202270435b1dc
NICOLA TORTORA <n.tortora10@studenti.unisa.it>
2023-04-14 20:10:55 +0200

    initialization test classes

---
+++ src/test/java/visitors/TypeVisitorTest.java
@@ -0,0 +1,86 @@
+package visitors;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TypeVisitorTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void visitBiExprNode() {
+    }
+
+    @Test
+    void visitUnExprNode() {
+    }
+
+    @Test
+    void visitConstExprNode() {
+    }
+
+    @Test
+    void getFromScope() {
+    }
+
+    @Test
+    void visitIdExprNode() {
+    }
+
+    @Test
+    void visitAssignOp() {
+    }
+
+    @Test
+    void visitProgramOp() {
+    }
+
+    @Test
+    void visitVarDeclOp() {
+    }
+
+    @Test
+    void visitFunOp() {
+    }
+
+    @Test
+    void visitBodyOp() {
+    }
+
+    @Test
+    void visitCallFunOp() {
+    }
+
+    @Test
+    void visitReturnStatOp() {
+    }
+
+    @Test
+    void visitIfStatOp() {
+    }
+
+    @Test
+    void visitWhileOp() {
+    }
+
+    @Test
+    void visitForStatOp() {
+    }
+
+    @Test
+    void visitReadOp() {
+    }
+
+    @Test
+    void visitWriteOp() {
+    }
+}
