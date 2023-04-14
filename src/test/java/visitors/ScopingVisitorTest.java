afb10aebd2964e4095e101faa0ac06ad834f8c25
NICOLA TORTORA <n.tortora10@studenti.unisa.it>
2023-04-14 19:58:02 +0200

    add junit dependency

---
+++ src/test/java/visitors/ScopingVisitorTest.java
@@ -0,0 +1,49 @@
+package visitors;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ScopingVisitorTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visitProgramOp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visitVarDeclOp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visitFunOp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visitParDeclOp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visitBodyOp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visitIfStatOp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visitWhileOp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void visitForStatOp() {
+    }
+}
