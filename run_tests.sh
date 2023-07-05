ee13043c2dc7a4b7fe3ed9c1925d9834ebbb3109
NICOLA TORTORA <n.tortora10@studenti.unisa.it>
2023-07-05 17:11:27 +0200

    update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,28 +1,42 @@
+#!/bin/bash
+
+# Assicuriamoci di avere il nome del progetto
+#!/bin/bash
+
 for EXEFILE in executables/valid*.exe; do
   TESTNAME=$(basename -- "$EXEFILE" .exe)
-  echo "" >> "$PROJECT_NAME.report.txt"
-  echo "" >> "$PROJECT_NAME.report.txt"
-  echo "Nome del test: $TESTNAME" >> "$PROJECT_NAME.report.txt"
-  echo "./$EXEFILE" >> "$PROJECT_NAME.report.txt"
-  echo "Output:" >> "$PROJECT_NAME.report.txt"
-  ./"$EXEFILE" >> "$PROJECT_NAME.report.txt" 2>&1
-  echo "" >> "$PROJECT_NAME.report.txt"
+  echo ""
+  echo "Nome del test: $TESTNAME"
+  echo "./$EXEFILE"
+
+  ./"$EXEFILE"
 
   for TESTIN in src/test/valid_in_out/$TESTNAME/${TESTNAME}_in.txt; do
-    TESTINNAME=$(basename -- "$TESTIN")
-    TESTOUT="src/test/valid_in_out/$TESTNAME/${TESTINNAME}_out.txt"
-    if [ -s "$EXEFILE" ]; then
-      echo "Esecuzione di ./"$EXEFILE" con input da $TESTIN:" >> "$PROJECT_NAME.report.txt"
+    if [ -f "$TESTIN" ]; then
+      TESTINNAME=$(basename -- "$TESTIN")
+      TESTOUT="src/test/valid_in_out/$TESTNAME/${TESTINNAME/_in/_out}.txt"
+
       ./"$EXEFILE" <"$TESTIN" &>"$TESTOUT"
-      cat "$TESTOUT" >> "$PROJECT_NAME.report.txt"
+
+      echo ""
+      echo "diff -w \"$TESTIN\" \"$TESTOUT\""
+
+      if diff -w "$TESTIN" "$TESTOUT"; then
+        echo "Test $TESTNAME su $TESTIN superato"
+      else
+        echo "Test $TESTNAME su $TESTIN fallito"
+      fi
+
+      echo ""
+      echo "Contenuto del file di output $TESTOUT:"
+      cat "$TESTOUT"
     else
-      RESULT=1
+      echo "File di input $TESTIN non trovato"
     fi
-    echo "" >> "$PROJECT_NAME.report.txt"
-    echo "diff -w \"${TESTIN/_in/_out}\" $TESTOUT" >> "$PROJECT_NAME.report.txt"
-    diff -w "$TESTIN" "$TESTOUT" >> "$PROJECT_NAME.report.txt"
   done
 done
+
+
 
 
 
